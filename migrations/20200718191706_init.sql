0a589e74e14c0fc23d5be5bb01c3e38e8aa91db0
redblueflame <contact@redblueflame.com>
2020-07-20 15:00:58 +0200

    feat(database): Added on pudate settings

--- migrations/20200718191706_init.sql
+++ migrations/20200718191706_init.sql
@@ -13,22 +13,22 @@ CREATE TABLE users
 );
 CREATE TABLE clients
 (
-    id             bigint PRIMARY KEY NOT NULL,
-    owner          bigint REFERENCES users,
-    name           varchar(32) NOT NULL,
+    id             bigint PRIMARY KEY                        NOT NULL,
+    owner          bigint REFERENCES users ON UPDATE CASCADE NOT NULL,
+    name           varchar(32)                               NOT NULL,
     -- The display_name can be null. In this case, the name takes place
     display_name   varchar(256),
     icon_url       varchar(2048),
-    public_key     varchar(256) NOT NULL,
-    secret_key     varchar(256) NOT NULL,
-    return_urls    varchar(1024)[] NOT NULL,
-    granted_scopes varchar(256)[] NOT NULL
+    public_key     varchar(256)                              NOT NULL,
+    secret_key     varchar(256)                              NOT NULL,
+    return_urls    varchar(1024)[]                           NOT NULL,
+    granted_scopes varchar(256)[]                            NOT NULL
 );
 CREATE TABLE refresh_tokens
 (
     -- The id is the token itself, returned to the user
-    id           varchar(36) PRIMARY KEY NOT NULL,
-    client_owner bigint REFERENCES clients NOT NULL,
-    "user"       bigint REFERENCES users NOT NULL,
-    usages       int NOT NULL
+    id           varchar(36) PRIMARY KEY                     NOT NULL,
+    client_owner bigint REFERENCES clients ON UPDATE CASCADE NOT NULL,
+    "user"       bigint REFERENCES users ON UPDATE CASCADE   NOT NULL,
+    usages       int                                         NOT NULL
 );
